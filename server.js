8c2e519930b946f67cc9c09169eb360773b87a6d
myang5t3r <matthew.yang@hunterdouglas.com>
2022-09-05 12:47:46 -0600

    Added unique id package to package.json, created endpoint for post /api/notes

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const { fstat } = require('fs');
 const fs = require('fs');
 const path = require('path');
+const suid = require('short-unique-id');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-
+// Must add middleware so we can handle request
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'))
 
 ///////////////         Routes for HTML         /////////////
@@ -20,19 +23,46 @@ app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes
 
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
 app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', (e, data) => {
+    fs.readFile('./db/db.json', 'utf8', (e, data) => {
         if (e) {
             console.log(e);
         } else {
             const notes = JSON.parse(data);
             res.json(notes);
-            console.log(notes);
+            // console.log(notes);
         }
     })
 })
 
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client, use a unique id when it saved;
 
+app.post('/api/notes', (req, res) => {
+    // let client know that the post was received
+    res.json(`${req.method} request was received`)
+    
+    // Lets create an unique id
+    const uid = new suid();
+    // Destructure response body
+    const { title, text } = req.body;
+    const newNote = {
+        title,
+        text,
+        id: uid()
+    }
+
+    console.log(JSON.stringify(newNote));
+    fs.readFile('./db/db.json', (e,data) => {
+    if (e) {
+        console.log(e);
+    }
+    else{
+        const notes = JSON.parse(data);
+        notes.push(newNote);
+        fs.writeFile('./db/db.json', JSON.stringify(notes), e => e ? console.log(e) : console.log(`Notes updated`))        
+    }
+
+   }) 
+})
 
 
 app.listen(PORT, () => console.log(`App listing on port ${PORT}`));
